5995e067a584234e4c93898734ade3132ea82898
Naimur Rahman <naimur.naiyimu@gmail.com>
2025-01-11 22:15:48 +0600

    Time: 7 ms (97.37%) | Memory: 20.1 MB (11.53%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,14 +1,19 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        n = len(ratings)
-        candies = [1] * n 
-
-        for i in range(1, n):
-            if ratings[i] > ratings[i-1]:
-                candies[i] = candies[i-1] + 1
-
-        for i in range(n-2, -1, -1):
-            if ratings[i] > ratings[i+1]:
-                candies[i] = max(candies[i], candies[i+1] + 1)
+        if not ratings:
+            return 0
         
-        return sum(candies)
+        ret, up, down, peak = 1, 0, 0, 0
+        
+        for prev, curr in zip(ratings[:-1], ratings[1:]):
+            if prev < curr:
+                up, down, peak = up + 1, 0, up + 1
+                ret += 1 + up
+            elif prev == curr:
+                up = down = peak = 0
+                ret += 1
+            else:
+                up, down = 0, down + 1
+                ret += 1 + down - int(peak >= down)
+        
+        return ret
